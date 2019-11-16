93e68ab48f04b37ebb12ff6c3e69c8f3f74a025d
jrjaskol <43866680+jrjaskol@users.noreply.github.com>
2019-11-16 17:36:18 -0500

    Header comments and first scan
    We should probably have different classes based on different topics in ethics

--- src/Aristotle.java
+++ src/Aristotle.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
+/*
+ * Aristotle chatbot 
+ * @Authors -- Josh Jaskolski, Chad Falkenberg, Taylor Hammes, Kallen Marcavage, Dawson McKenzie
+ * 
+ * This bot is designed to take in user input, analyze the words used, and make a reply roughly based on what was said
+ * The focus of the topics of the bot is ethics, specifically the ethical ideas that Aristotle invented
+ * 
+ * Last edited -- 5:34 11/16
+ */
 
 public class Aristotle {
 
-	int x = 69;
+	
+	public static void main(String[] args){
+		
+		System.out.println("Hello my child, I am Aristotle, what would you like to talk about? ");
+		
+		Scanner in = new Scanner(System.in);
+		String input = in.nextLine();
+		
+		System.out.println(input);
+	}
 }
